f9082758ed4555cd30ae86a138f0a63844e506ea
Ryan Casey <ryepdx@gmail.com>
2013-01-29 13:26:44 -0800

    Added another test and changed the numpy exception handling to throw exceptions instead of warnings, allowing us to catch and log them as we like.

--- async_tasks/buffs/tests.py
+++ async_tasks/buffs/tests.py
@@ -11,6 +11,11 @@ class MockModel(object):
     @classmethod
     def get_data(cls, instance):
         return instance['data']
+        
+class MockModel2(MockModel):
+    @classmethod
+    def get_collection(cls, *args, **kwargs):
+        return MockCollection2()
 
 class MockCollection(object):
     def set_data(self, data):
@@ -32,6 +37,20 @@ class MockCollection(object):
                 {'data': 1.0, 'interval_start': '2013-01-05'},
             ]
 
+class MockCollection2(MockCollection):
+    def sort(self, *args, **kwargs):
+        if hasattr(self, 'data'):
+            return self.data
+        else:
+            return [
+                {'data': 2.0, 'interval_start': '2012-12-01'},
+                {'data': 1.0, 'interval_start': '2012-12-08'},
+                {'data': 4.0, 'interval_start': '2012-12-15'},
+                {'data': 1.0, 'interval_start': '2012-12-22'},
+                {'data': 5.0, 'interval_start': '2012-12-29'},
+                {'data': 3.0, 'interval_start': '2013-01-05'},
+            ]
+
 class TestCorrelationTask(CorrelationTask):
     @property
     def required_aspects(self):
@@ -48,6 +67,12 @@ class TestCorrelationTask(CorrelationTask):
             
     def save_buffs(self, buffs):
         self.buffs = buffs
+
+class TestCorrelationTask2(TestCorrelationTask):
+    @property
+    def required_aspects(self):
+        return {'google_tasks': ('completed_task', MockModel),
+                 'lastfm': ('song_energy', MockModel)}
 
 class TestCorrelationTaskClass(unittest.TestCase):        
     def given_a_default_test_task(self):
@@ -69,3 +94,9 @@ class TestCorrelationTaskClass(unittest.TestCase):
                 self.given_a_default_test_task()
             )
         )
+        
+class TestCorrelationTaskClass2(TestCorrelationTaskClass):        
+    def given_a_default_test_task(self):
+        return TestCorrelationTask2(
+            {'_id':ObjectId('50e3da15ab0ddcff7dd3c187')},
+            ['google_tasks', 'lastfm'])
